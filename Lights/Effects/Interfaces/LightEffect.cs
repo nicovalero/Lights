cc0895dbe77de9a92dbcb0a3c02894fa6ca5d9b3
Nico Valero <Nico Valero@DESKTOP-QVSC2O5>
2022-10-22 11:32:00 +0100

    DoubleColumnListConfigCard added. Removed old API project

--- Lights/Effects/Interfaces/LightEffect.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PhilipsHueAPI.Effects.Interfaces
-{
-    public interface LightEffect
-    {
-        public void Perform(List<string> lightIds, object value);
-    }
-}
---
+++ UI/User Controls/DoubleColumnListConfigCard.xaml.cs
@@ -0,0 +1,54 @@
+﻿using MIDI.Models.Structs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using UI.Models.Structs;
+
+namespace UI.User_Controls
+{
+    /// <summary>
+    /// Interaction logic for DoubleColumnListConfigCard.xaml
+    /// </summary>
+    public partial class DoubleColumnListConfigCard : UserControl
+    {
+        public DoubleColumnListConfigCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(DoubleColumnListConfigCard));
+
+        public Color CardBackground
+        {
+            get { return (Color)GetValue(CardBackgroundProperty); }
+            set { SetValue(CardBackgroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty CardBackgroundProperty = DependencyProperty.Register("CardBackground", typeof(Color), typeof(DoubleColumnListConfigCard));
+
+        internal List<SimpleConfigList_ViewModel> Source
+        {
+            get { return (List<SimpleConfigList_ViewModel>)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        internal static readonly DependencyProperty SourceProperty = DependencyProperty.Register("Source", typeof(List<SimpleConfigList_ViewModel>), typeof(DoubleColumnListConfigCard));
+    }
+}
