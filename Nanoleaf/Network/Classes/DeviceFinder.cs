208607a0bdac8b5b54a0058ff868356f36fbd44d
Nico Valero <Nico Valero@DESKTOP-QVSC2O5>
2023-12-17 00:40:44 +0000

    Nanoleaf project start

---
+++ Nanoleaf/Network/Classes/DeviceFinder.cs
@@ -0,0 +1,13 @@
+﻿using Nanoleaf.Network.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nanoleaf.Network.Classes
+{
+    internal class DeviceFinder: IDeviceFinder
+    {
+    }
+}
---
+++ Nanoleaf/Network/Classes/HTTPMessenger.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Nanoleaf.Network
+{
+    internal static class HTTPMessenger
+    {
+        private static HttpClient _client = new HttpClient();
+
+        internal static HttpResponseMessage SendGetRequestAsync(string path)
+        {
+            return _client.GetAsync(path).Result;
+        }
+
+        internal static async Task<HttpResponseMessage> SendPostRequestAsync(string path, StringContent data = null)
+        {
+            return await _client.PostAsync(path, data);
+        }
+
+        internal static async Task<HttpResponseMessage> SendPutRequestAsync(string path, StringContent data = null)
+        {
+            return await _client.PutAsync(path, data);
+        }
+    }
+}
